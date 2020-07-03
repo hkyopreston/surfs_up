2bf39ceeaa15b0c2a77546e84984c92f2511c94e
Harrison Preston <harrisonpreston@Harrisons-MacBook-Pro.local>
2020-07-03 12:47:05 -0700

    set up climate app using flask

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+import app
+
+print("example __name__ = %s", __name__)
+
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
